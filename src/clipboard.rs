91d04095602a0b49a61997eb21ad44d26d7d0f4f
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2021-07-09 17:46:18 +0900

    Fix CI error (#18)
    * fix ci error
    
    * install dependencies
    
    * remove linux feature
    
    * remove cfg os
    
    * default feature false
    
    * fix dependencies
    
    * install dependencies
    
    * fix dependencies
    
    * fix dependencies
    
    * fix dependencies
    
    * add sudo
    
    * fix dependencies
    
    * remove unneeded dependencies

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,8 +1,3 @@
-#[cfg(any(test, not(any(target_os = "linux", target_os = "macos", windows))))]
-use copypasta::nop_clipboard::NopClipboardContext;
-#[cfg(target_os = "linux")]
-use copypasta::x11_clipboard::{Primary as X11SelectionClipboard, X11ClipboardContext};
-#[cfg(any(target_os = "linux", target_os = "macos", windows))]
 use copypasta::ClipboardContext;
 use copypasta::ClipboardProvider;
 
@@ -12,38 +7,36 @@ pub struct Clipboard {
 }
 
 impl Clipboard {
-    #[cfg(any(target_os = "linux", target_os = "macos", windows))]
     pub fn new() -> Self {
         Self::default()
     }
 
-    #[cfg(any(test, not(any(target_os = "linux", target_os = "macos", windows))))]
-    pub fn new_nop() -> Self {
-        Self {
-            clipboard: Box::new(NopClipboardContext::new().unwrap()),
-            selection: None,
-        }
-    }
+    // #[cfg(any(test, not(any(target_os = "macos", windows))))]
+    // pub fn new_nop() -> Self {
+    //     Self {
+    //         clipboard: Box::new(NopClipboardContext::new().unwrap()),
+    //         selection: None,
+    //     }
+    // }
 }
 
 impl Default for Clipboard {
     fn default() -> Self {
-        #[cfg(any(target_os = "macos", windows))]
         return Self {
             clipboard: Box::new(ClipboardContext::new().unwrap()),
             selection: None,
         };
 
-        #[cfg(target_os = "linux")]
-        return Self {
-            clipboard: Box::new(ClipboardContext::new().unwrap()),
-            selection: Some(Box::new(
-                X11ClipboardContext::<X11SelectionClipboard>::new().unwrap(),
-            )),
-        };
+        // #[cfg(target_os = "linux")]
+        // return Self {
+        //     clipboard: Box::new(ClipboardContext::new().unwrap()),
+        //     selection: Some(Box::new(
+        //         X11ClipboardContext::<X11SelectionClipboard>::new().unwrap(),
+        //     )),
+        // };
 
-        #[cfg(not(any(target_os = "linux", target_os = "macos", windows)))]
-        return Self::new_nop();
+        // #[cfg(not(any(target_os = "macos", windows)))]
+        // return Self::new_nop();
     }
 }
 
