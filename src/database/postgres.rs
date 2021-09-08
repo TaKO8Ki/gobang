cd1f31203d5a9b906c5fb725969dfd0b8814ada9
Takayuki Maeda <takoyaki0316@gmail.com>
2021-09-08 12:52:36 +0900

    add number type to postgres

--- src/database/postgres.rs
+++ src/database/postgres.rs
@@ -252,7 +252,14 @@ impl Pool for PostgresPool {
                                 serde_json::Value::Array(v) => {
                                     new_row.push(v.iter().map(|v| v.to_string()).join(","))
                                 }
-                                _ => (),
+                                serde_json::Value::Number(v) => new_row.push(v.to_string()),
+                                others => {
+                                    panic!(
+                                        "column type not implemented: `{}` {}",
+                                        column.name(),
+                                        others
+                                    )
+                                }
                             }
                         }
                     }
